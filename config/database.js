8a7b20c08f3ef49675297612e378fba1fa901e2b
Dang Xuan Phuc <22T10207022husc.edu.vn>
2026-01-30 21:16:48 +0700

    fix_ver

--- config/database.js
+++ config/database.js
@@ -1,36 +1,10 @@
-// const mongoose = require("mongoose");
-
-// module.exports.connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL);
-//     console.log("Connet Success");
-//   } catch (error) {
-//     console.log("Connet Error");
-//   }
-// }
-
-
 const mongoose = require("mongoose");
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connect() {
-  if (cached.conn) {
-    return cached.conn;
+module.exports.connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connet Success");
+  } catch (error) {
+    console.log("Connet Error");
   }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URL, {
-      bufferCommands: false,
-    }).then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
-
-module.exports = { connect };
